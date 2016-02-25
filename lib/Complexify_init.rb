0a6623fe0564dfd3cf0ffb65eadbef69b2a79101
Lovelyn <lovelynisrael@hotmail.com>
2016-02-25 08:19:25 +0100

    Modify Readme file, complex_num.rb and Complexify._init.rb files

--- lib/Complexify_init.rb
+++ lib/Complexify_init.rb
@@ -1,4 +1,4 @@
-require_relative "../Complexify/complex_num.rb"
+require_relative "Complexify/complex_num"
 
 class StartApp
   def start
@@ -10,25 +10,45 @@ class StartApp
          "Here are some helpful keys to navigate the app:\n\n"\
          "'re' - real\n\n"\
          "'img' - imaginary\n\n"
-    puts "How many operands will you be using?"
-    operand_num = gets.chomp.to_i
+    get_operand_num
+    get_complex_num
+  end
 
-    result = ""
-    operand_num.times do
-        operand_num_real = get_number("Enter the 're' part of no. #{operand_num} complex number ")
-        operand_num_imag = get_number("Enter the  part of first complex number ")
-        @real = operand_num_real
-        @imaginary = operand_num_imag
-     result = Math::Complexify::Complex.new(operand_num_real, operand_num_imag)
+  def get_operand_num
+    puts "How many operands will you be using?"
+  end
+
+  def get_complex_num
+    loop do
+      operand_num = gets.chomp.to_i
+      result = ""
+      if operand_num > 1
+        count = 1
+        operand_num.times do
+          operand_num_real = get_number("Enter the 're' part of no. #{count} complex number ")
+          operand_num_imag = get_number("Enter the 'img' part of no. #{count} complex number ")
+          @real = operand_num_real
+          @imaginary = operand_num_imag
+          result = Math::Complexify::Complex.new(operand_num_real, operand_num_imag)
+          count += 1
+        end
+      elsif operand_num <= 1
+        puts "You cannot have #{operand_num} operand"
+        get_operand_num
+        break
+      end
+      puts result
     end
   end
 
   def get_number(prompt)
     result = nil
     loop do
-      puts number
+      puts prompt
       result = gets.chomp.to_i
       break if result.class != "Fixnum"
     end
   end
 end
+
+StartApp.new.start
